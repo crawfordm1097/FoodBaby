2f37ef5d0d12b7be2fee7b3a927ae6f245d67029
Brady Ochse <bradyoch@gmail.com>
2018-10-25 10:28:33 -0400

    Added locations to another document and updated the schemas to match

--- db/db.js
+++ db/db.js
@@ -3,14 +3,15 @@
 const config = require('./config.js'),
       mongoose = require('mongoose'),
       User = require('./control/user.control.js'),
-      Listing = require('./control/listing.control.js');
+      Listing = require('./control/listing.control.js'),
+      Location = require('./control/location.control.js');
 
 mongoose.connect(config.db.uri, { useNewUrlParser: true });
-
 
 var db = {};
 
 db.users = User;
 db.listings = Listing;
+db.locations = Location;
 
 module.exports = db;
