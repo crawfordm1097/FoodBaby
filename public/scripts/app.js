c9ab6038c6daab2dfbac4da523b6880732606cce
Mikayla Crawford <crawfordm1097@gmail.com>
2018-10-29 20:56:02 -0400

    Add map markers

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,38 @@ var app = angular.module('foodBaby', []);
 
 app.controller('ListingsCtrl', ($scope, $http) => {
   $http.get('/api/listings').then((response) => {
-    $scope.listings = response.data;
+      $scope.listings = response.data;
+      var recentevents = $scope.listings.slice(0, 20);
+      var geojson;
+      var fts = [];
+
+      //create feature json for each event
+      for (var i = 0; i < recentevents.length; i++) {
+          fts[i] = {
+              type: 'feature',
+              geometry: {
+                  type: 'point',
+                  coordinates: [recentevents[i].location.coordinates.latitude, recentevents[i].location.coordinates.longitude]
+              },
+              properties: {
+                  title: recentevents[i].name,
+                  food_type: recentevents[i].food_type,
+                  time: {
+                      start: recentevents[i].time.start,
+                      end: recentevents[i].time.end
+                  },
+                  location: recentevents[i].location.address
+              }
+          };
+      }
+
+      //create geojson
+      geojson = {
+          type: 'featurecollection',
+          features: fts
+      };
+
+      //createMarker(geojson); //Call create marker (defined in mapbox.js)
   }, (error) => {
     console.log('Unable to retrieve listings: ', error);
   });
@@ -16,5 +47,6 @@ app.controller('ListingsCtrl', ($scope, $http) => {
   $scope.view = function(id) {
     window.location = `/api/listings/id/${id}`;
   }
+
 });
 
--- public/scripts/mapbox.js
+++ public/scripts/mapbox.js
@@ -6,3 +6,56 @@ var map = new mapboxgl.Map({
   zoom: 13.6868419678297,
   minZoom: 13
 });
+
+//test
+var geojson = {
+    type: 'featurecollection',
+    features: [{
+        type: 'feature',
+        geometry: {
+            type: 'point',
+            coordinates: [-82.343900, 29.649300]
+        },
+        properties: {
+            title: 'pizza 4 brek !!?!?',
+            location: 'turlington hall',
+            time: {
+                start: '8am',
+                end: '10am'
+            },
+            food_type: 'pizza'
+        }
+    },
+    {
+        type: 'feature',
+        geometry: {
+            type: 'point',
+            coordinates: [-82.361300, 29.643100]
+        },
+        properties: {
+            title: 'dinner w the chompers',
+            location: 'lake alice',
+            time: {
+                start: '4pm',
+                end: '8pm'
+            },
+            food_type: 'stuffed quail'
+        }
+    }]
+};
+
+createMarker(geojson);
+
+//add markers to the map for every event
+function createMarker(geojson) {
+    console.log(geojson.features);
+    geojson.features.forEach(function (marker) {
+        new mapboxgl.Marker({ color: "000000" })
+          .setLngLat(marker.geometry.coordinates)
+          .setPopup(new mapboxgl.Popup({ offset: 25 }) //add popups
+          .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.location + '</p><p>'
+                        + marker.properties.time.start + ' - ' + marker.properties.time.end + '</p><p>'
+                        + marker.properties.food_type + '</p>'))
+          .addTo(map);
+    });
+}
