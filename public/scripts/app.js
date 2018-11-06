5a9902077e9fa88c87f4519b832b4a3437cc40b8
Mikayla Crawford <crawfordm1097@gmail.com>
2018-11-06 10:35:36 -0500

    Merge mapbox.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -66,16 +66,18 @@ app.controller('SignUpController',  function($scope, $http){
 });
 
 app.directive("mapbox", function() {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiZm9vZGJhYnlnMiIsImEiOiJjam10YTdtNjAwNWg2M3dwMWw3am14emhzIn0.dlnV1DEKRSxnKRwa7I2qLw';
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10',
-    center: [-82.35256285341353, 29.641654178244437],
-    zoom: 13.6868419678297,
-    minZoom: 13
-  });
   return {
-    restrict: 'E',
+      restrict: 'E',
+      link: function (scope, element, attributes) {
+          mapboxgl.accessToken = 'pk.eyJ1IjoiZm9vZGJhYnlnMiIsImEiOiJjam10YTdtNjAwNWg2M3dwMWw3am14emhzIn0.dlnV1DEKRSxnKRwa7I2qLw';
+          var map = new mapboxgl.Map({
+              container: 'map',
+              style: 'mapbox://styles/mapbox/streets-v10',
+              center: [-82.35256285341353, 29.641654178244437],
+              zoom: 13.6868419678297,
+              minZoom: 13
+          });
+      }
   }
 
 })
