9f091daef517f68e62e6996182859777852c79c8
nimitp <npatel1@ufl.edu>
2018-11-07 10:51:08 -0500

    login redirection

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,13 @@
 module.exports = function(app, passport) {
 
-    app.post('/login', passport.authenticate('local-signup', {
-        successRedirect : '/profile',            // redirect to the secure profile section
-        failureRedirect : '/login',              // redirect back to the login page if there is an error
+    app.post('/user/login', passport.authenticate('local', {
+        successRedirect : '/user/profile',       // redirect to the secure profile section
+        failureRedirect : '/user/login',         // redirect back to the login page if there is an error
         failureFlash : true                      // allow flash messages
     }));
    
 };
+
+
+
+
